d730049892d1a40f0b3e00a0f7599f3f4283523c
владимир штурмин <vova_shturmin98@mail.ru>
2022-07-28 21:26:49 +0300

    Add base car model

---
+++ CarFuelCalc.Data/Models/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace CarFuelCalc.Data.Models;
+public class Car
+{
+    public string CarType { get; private set; }
+    public double AverageFuelConsumtion { get; private set; }
+    public int FuelCapacity { get; private set; }
+    public double CurrentFuelLevel { get; private set; }
+    public int Speed { get; private set; }
+
+    public Car(int speed, int fuelCapacity, double currentFuelLevel, double averageFuelConsumtion)
+    {
+        Speed = speed;
+        FuelCapacity = fuelCapacity;
+        CurrentFuelLevel = currentFuelLevel;
+        AverageFuelConsumtion = averageFuelConsumtion;
+    }
+
+  
+
+    public virtual double GetRemainDistance(bool calculateForFullCapacity)
+    {
+        if (calculateForFullCapacity)
+        {
+            return FuelCapacity / AverageFuelConsumtion*100;
+        }
+        else
+        {
+            return CurrentFuelLevel / AverageFuelConsumtion*100;
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using CarFuelCalc.Data.Models;
+
+var car = new Car(100, 67, 43, 5.3);
+
+Console.WriteLine(car.GetRemainDistance(true));
+Console.WriteLine(car.GetRemainDistance(false));
